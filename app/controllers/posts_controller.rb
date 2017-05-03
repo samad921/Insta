7537efce0c02aba5d3eef3b9e70136d99c0c20fd
Abdul Samad <samad921@hotmail.com>
2017-05-03 10:16:08 +0500

    First Part OF Insta Clone Has Been Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-
+	
+before_action :set_post, only: [:show, :edit, :update, :destroy] 
 def index  
  @posts = Post.all
 end 
@@ -7,28 +8,43 @@ def new
     @post = Post.new
 end
 
-def create  
-  @post = Post.create(post_params)
-  redirect_to posts_path
-end   
+def create
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      render :new
+    end
+  end  
+
 def show  
-  @post = Post.find(params[:id])
 end  
 
 def edit 
- @post = Post.find(params[:id]) 
 end
 
 def update
- @post = Post.find(params[:id])
- @post.update(post_params)
- redirect_to(post_path(@post))  
-end
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed.  Please check the form."
+      render :edit
+    end
+  end
 
+def destroy  
+  @post.destroy
+  redirect_to posts_path
+end 
 private
 
 def post_params  
   params.require(:post).permit(:image, :caption)
 end 
 
+def set_post
+	@post = Post.find(params[:id])
+end
 end
